b5baf6204f5136203d5730b86b3e6454fb4a0071
Vitaliy <vito.co@ya.ru>
2023-05-24 00:25:38 +0300

    - make lecture model with megration, factory and seeder

---
+++ database/factories/LectureFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Lecture>
+ */
+class LectureFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
