c7fbc33f262edfb579f36517884c193a1d74be87
Vitaliy <vito.co@ya.ru>
2023-05-24 00:42:53 +0300

    - one to many relationship for student and classroom models

--- app/Models/Classroom.php
+++ app/Models/Classroom.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Classroom extends Model
 {
@@ -12,4 +13,9 @@ class Classroom extends Model
     protected $fillable = [
         'title',
     ];
+
+    public function students(): HasMany
+    {
+        return $this->hasMany(Student::class);
+    }
 }
--- app/Models/Student.php
+++ app/Models/Student.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Student extends Model
 {
@@ -12,5 +13,11 @@ class Student extends Model
     protected $fillable = [
         'name',
         'email',
+        'classroom_id',
     ];
+
+    public function classroom(): BelongsTo
+    {
+        return $this->belongsTo(Classroom::class);
+    }
 }
