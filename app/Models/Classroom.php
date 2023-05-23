81248dc7db3dd42d37887f72741a0fd2efe1421a
Vitaliy <vito.co@ya.ru>
2023-05-24 00:15:08 +0300

    - make classroom model with megration, factory and seeder

--- app/Models/Classroom.php
+++ app/Models/Classroom.php
@@ -5,12 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Student extends Model
+class Classroom extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'name',
-        'email',
+        'title',
     ];
 }
